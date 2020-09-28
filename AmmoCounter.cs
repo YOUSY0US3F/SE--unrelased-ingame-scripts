49679bb776e800588149f0702bc0b4fbff66388f
YousefAttia-git <72047433+YousefAttia-git@users.noreply.github.com>
2020-09-28 17:27:12 -0400

    Add files via upload

---
+++ AmmoCounter.cs
@@ -0,0 +1,201 @@
+﻿/*
+ * R e a d m e
+ * -----------
+ * 
+ * In this file you can include any instructions or other comments you want to have injected onto the 
+ * top of your final script. You can safely delete this file if you do not want any such comments.
+ */
+
+
+List<CargoItems> AmmoComps = new List<CargoItems>()
+{
+    new CargoItems("SteelPlate",0),
+    new CargoItems("Motor",0),
+    new CargoItems("PowerCell",0),
+    new CargoItems("Construction",0),
+    new CargoItems("Computer",0),
+    new CargoItems("Superconductor",0),
+    new CargoItems("LargeTube",0),
+    new CargoItems("GravityGenerator",0),
+};
+//initialize variables
+IMyTextPanel Comp;
+IMyTextPanel AmmoCount;
+CargoItems temp;
+public Program()
+{
+    //updates every 100 ticks
+    Runtime.UpdateFrequency=UpdateFrequency.Update100;
+
+    AmmoCount = GridTerminalSystem.GetBlockWithName("AmmoCount") as IMyTextPanel;
+    Comp = GridTerminalSystem.GetBlockWithName("Comp") as IMyTextPanel;
+    temp = new CargoItems("name", 0);
+
+}
+//linearly searches through all containers in the grid for all items required to make a shell, gets the count of each item
+public void CountItems()
+{
+    List<IMyTerminalBlock> Cargo = new List<IMyTerminalBlock>();
+    List<MyInventoryItem> itemList = new List<MyInventoryItem>();
+    GridTerminalSystem.GetBlocksOfType<IMyEntity>(Cargo, block => block.InventoryCount > 0);
+    foreach (IMyTerminalBlock CBlock in Cargo)
+    {
+
+        itemList.Clear();
+        CBlock.GetInventory(0).GetItems(itemList, item => item.Type.SubtypeId.Equals("SteelPlate") || item.Type.SubtypeId.Equals("Motor") ||
+        item.Type.SubtypeId.Equals("PowerCell") || item.Type.SubtypeId.Equals("Computer") || item.Type.SubtypeId.Equals("Construction") ||
+        item.Type.SubtypeId.Equals("LargeTube") || item.Type.SubtypeId.Equals("Superconductor") || item.Type.SubtypeId.Equals("GravityGenerator"));
+
+        foreach (MyInventoryItem item in itemList)
+        {
+            for (int i = 0; i < AmmoComps.Count; i++)
+            {
+                AmmoComps[i].CompareAndAdd(item);
+            }
+        }
+
+        if (CBlock.InventoryCount > 1)
+        {
+            itemList.Clear();
+            CBlock.GetInventory(1).GetItems(itemList, item => item.Type.SubtypeId.Equals("SteelPlate") || item.Type.SubtypeId.Equals("Motor") ||
+            item.Type.SubtypeId.Equals("PowerCell") || item.Type.SubtypeId.Equals("Computer") || item.Type.SubtypeId.Equals("Construction") ||
+            item.Type.SubtypeId.Equals("LargeTube") || item.Type.SubtypeId.Equals("Superconductor") || item.Type.SubtypeId.Equals("GravityGenerator"));
+
+            foreach (MyInventoryItem item in itemList)
+            {
+                for (int i = 0; i < AmmoComps.Count; i++)
+                {
+                    AmmoComps[i].CompareAndAdd(item);
+                }
+            }
+        }
+    }
+}
+
+public void ResetCount()
+{
+    for (int i = 0; i < AmmoComps.Count; i++)
+    {
+        AmmoComps[i].Clear();
+    }
+}
+//calculates the average amount of shells you can make with the amount of items you have
+public int ShellAmt(List<CargoItems> list)
+{
+    double sum = 0;
+
+    foreach(CargoItems item in list)
+    {
+        if(item.GetName().Equals("SteelPlate"))
+        {
+            sum += item.GetAmount() / 182;
+        }
+
+        if (item.GetName().Equals("Motor"))
+        {
+            sum += item.GetAmount() / 2;
+        }
+
+        if (item.GetName().Equals("PowerCell"))
+        {
+            sum += item.GetAmount() / 80;
+        }
+
+        if (item.GetName().Equals("Construction"))
+        {
+            sum += item.GetAmount() / 75;
+        }
+
+        if (item.GetName().Equals("Computer"))
+        {
+            sum += item.GetAmount() / 47;
+        }
+
+        if (item.GetName().Equals("Superconductor"))
+        {
+            sum += item.GetAmount() / 20;
+        }
+
+        if (item.GetName().Equals("LargeTube"))
+        {
+            sum += item.GetAmount() / 6;
+        }
+
+        if (item.GetName().Equals("GravityGenerator"))
+        {
+            sum += item.GetAmount() / 9;
+        }
+
+        if (item.GetAmount() == 0)
+        {
+            return 0;
+        }
+    }
+
+    return (int) Math.Floor(sum/8);
+}
+//prints out each item and its amount
+public string ToString(List<CargoItems> list)
+{
+    string output="";
+    foreach(CargoItems item in list)
+    {
+        output += item.GetName() + ": " + item.GetAmount() + "\n";
+    }
+
+    return output;
+}
+
+
+
+public void Main(string argument, UpdateType updateSource)
+{
+    ResetCount();
+    CountItems();
+
+    AmmoCount.WriteText("Ammo Remaining: " + ShellAmt(AmmoComps));
+    Comp.WriteText(ToString(AmmoComps));
+}
+
+public class CargoItems
+{
+    public string ItemName;
+    public double Quantity;
+
+    public CargoItems(string name, double amount)
+    {
+        ItemName = name;
+
+        Quantity = amount;
+    }
+
+    public double GetAmount()
+    {
+        return Quantity;
+    }
+
+    public String GetName()
+    {
+        return ItemName;
+    }
+
+    public void AddTo(double amt)
+    {
+        Quantity += amt;
+    }
+
+    public void Clear()
+    {
+        Quantity = 0;
+    }
+
+    public void CompareAndAdd(MyInventoryItem item)
+    {
+        if (item.Type.SubtypeId.Equals(ItemName))
+        {
+            Quantity += (double)item.Amount;
+        }
+    }
+
+
+}
---
+++ SolarArray.cs
@@ -0,0 +1,136 @@
+﻿/*
+ * R e a d m e
+ * -----------
+ * 
+ * In this file you can include any instructions or other comments you want to have injected onto the 
+ * top of your final script. You can safely delete this file if you do not want any such comments.
+ */
+
+//this script takes two rotors and a solor panel and ajusts the solar panel to face the sun
+IMySolarPanel panel;
+
+IMyMotorStator pivot, rot , rotslave , pivotslave;
+
+bool pivotIsAligned, pivotIsMoving, pivotRightWay = false;
+
+bool rotIsAligned, rotIsMoving, rotRightWay = false;
+
+double lastOutput,finalOutput = 0;
+List<IMyTerminalBlock> pivots;
+List<IMyTerminalBlock> rots;
+IMyBlockGroup rotors;
+
+public Program()
+{
+    //updates every tick
+    Runtime.UpdateFrequency = UpdateFrequency.Update1;
+    pivots = new List<IMyTerminalBlock>();
+    rots = new List<IMyTerminalBlock>();
+    rotors = GridTerminalSystem.GetBlockGroupWithName("pivots");
+    if (rotors != null)
+    {
+        rotors.GetBlocks(pivots);
+    }
+    //if there are no detected rotors to slave the script will only use the one solar panel
+    else
+    {
+        Echo(" no pivots group found, going solo");
+    }
+    rotors = GridTerminalSystem.GetBlockGroupWithName("rots");
+    if (rotors != null)
+    {
+        rotors.GetBlocks(rots);
+    }
+    else
+    {
+        Echo(" no rots group found, going solo");
+    }
+
+    panel = GridTerminalSystem.GetBlockWithName("guide") as IMySolarPanel;
+    pivot = GridTerminalSystem.GetBlockWithName("pivot") as IMyMotorStator;
+    rot = GridTerminalSystem.GetBlockWithName("rot") as IMyMotorStator;
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+    //checks if the panel is aligned
+    if (!pivotIsAligned)
+    {
+        //if the panel is not moving it will move in the opposite direction of its last movement
+        if (!pivotIsMoving)
+        {
+            pivotIsMoving = true;
+            pivot.TargetVelocityRPM = -1;
+        }
+        //checks if the panels output is lower this time around
+        if (panel.MaxOutput < lastOutput)
+        {
+            pivot.TargetVelocityRPM = -pivot.TargetVelocityRPM;
+            //if it is not facing the right way it will go through the main method from the start
+            if (pivotRightWay)
+            {
+                pivotIsAligned = true;
+                pivotIsMoving = false;
+                pivot.TargetVelocityRPM = 0;
+                lastOutput = 0;
+                finalOutput = panel.MaxOutput;
+            }
+            else
+            {
+                pivotRightWay = true;
+            }
+        }
+    }
+    //this does the samething the pivot part did but for the rotational (aka Azimuth) axis
+    if (!rotIsAligned)
+    {
+        if (!rotIsMoving)
+        {
+            rotIsMoving = true;
+            rot.TargetVelocityRPM = -1;
+        }
+
+        if (panel.MaxOutput < lastOutput)
+        {
+            rot.TargetVelocityRPM = -rot.TargetVelocityRPM;
+
+            if (rotRightWay)
+            {
+                rotIsAligned = true;
+                rotIsMoving = false;
+                rot.TargetVelocityRPM = 0;
+                lastOutput = 0;
+                pivotIsAligned = false;
+            }
+            else
+            {
+                rotRightWay = true;
+            }
+        }
+    }
+
+    //if the maxoutput is less than 90% of the current output we go through the whole main method again
+    if (rotIsAligned && pivotIsAligned && panel.MaxOutput < finalOutput*0.9 )
+    {
+        rotIsAligned = false;
+        pivotIsAligned = false;
+        pivotRightWay = false;
+        pivotRightWay = false;
+
+
+
+    }
+    //once everything is aligned and the slaved solar panels exist, we align the slaved solar panels with the guide
+    if (rotIsAligned && pivotIsAligned && rotors!= null)
+    {
+        for (int i = 0; i < pivots.Count(); i++)
+        {
+            pivotslave = (IMyMotorStator)pivots[i];
+            rotslave = (IMyMotorStator)rots[i];
+            pivotslave.TargetVelocityRad = pivot.Angle - pivotslave.Angle;
+            rotslave.TargetVelocityRad = rot.Angle - rotslave.Angle;
+        }
+    }
+    //store current output
+    lastOutput = panel.MaxOutput;
+}

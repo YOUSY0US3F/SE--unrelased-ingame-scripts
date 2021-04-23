a1e2395ce6b51ffef6cf79a96bcd9b2314745b95
YousefAttia-git <72047433+YousefAttia-git@users.noreply.github.com>
2021-04-23 18:33:32 -0400

    Update SolarArray.cs

--- SolarArray.cs
+++ SolarArray.cs
@@ -1,10 +1,4 @@
-﻿/*
- * R e a d m e
- * -----------
- * 
- * In this file you can include any instructions or other comments you want to have injected onto the 
- * top of your final script. You can safely delete this file if you do not want any such comments.
- */
+
 
 //this script takes two rotors and a solor panel and ajusts the solar panel to face the sun
 IMySolarPanel panel;

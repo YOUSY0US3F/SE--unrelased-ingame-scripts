06924f0f936bf4aadeac7f4778feb7391f67ff77
YousefAttia-git <72047433+YousefAttia-git@users.noreply.github.com>
2021-04-23 18:41:10 -0400

    Update Ingame Chatroom.cs

--- Ingame Chatroom.cs
+++ Ingame Chatroom.cs
@@ -1,10 +1,3 @@
-﻿/*
- * R e a d m e
- * -----------
- * 
- * In this file you can include any instructions or other comments you want to have injected onto the 
- * top of your final script. You can safely delete this file if you do not want any such comments.
- */
 
 //This script takes your arguments ans sends it to all programmable blocks using this script as a meesage
 //declare variables
